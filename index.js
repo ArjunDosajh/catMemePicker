edcde6a61f001191136d1b57fba7efb288529bef
ArjunDosajh <arjundosajh100@gmail.com>
2023-01-19 10:59:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { catsData } from './data.js'
+import { catsData } from 'https://arjundosajh.github.io/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
